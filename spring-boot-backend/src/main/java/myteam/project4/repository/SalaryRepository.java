ceebf57f3f50f9daf3047b241211feb0ef616db2
Minh Chau <chimsetocxu211427@gmail.com>
2021-12-12 22:39:27 +0700

    init project

---
+++ spring-boot-backend/src/main/java/myteam/project4/repository/SalaryRepository.java
@@ -0,0 +1,19 @@
+package myteam.project4.repository;
+
+import myteam.project4.entity.Salary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SalaryRepository extends JpaRepository<Salary, Long> {
+    List<Salary> findByIsDeletedAndPositionLike(Boolean isDeleted, String position);
+    List<Salary> findAllByIsDeleted(boolean isDeleted);
+    Salary findById(int id);
+    Optional<Salary> findByPositionAndLevel(String position, String level);
+    @Query(value = "SELECT distinct (s.position) FROM Salary s")
+    List<String> findAllPosition();
+    @Query(value = "SELECT s.level FROM Salary  s WHERE s.position = :position")
+    List<String> findAllLevelByPosition(String position);
+}
